d04a8f0b8089b9180dbae9f6c010eb6315762f82
Edwin J <64994681+ed963@users.noreply.github.com>
2023-01-21 13:03:42 -0500

    Add usage example for query.py

--- utils/query.py
+++ utils/query.py
@@ -1,3 +1,22 @@
+"""
+Python script for getting definitions and examples from Urban Dictionary API.
+
+Usage: 
+    python3 query.py <search_term>
+
+Example:
+$ python3 query.py rizz
+Word: rizz
+Definition: A word for having an elite amount of game, specifically for getting with girls.
+Example: "Holly Spragg has impeccable rizz and pulls too many bitches".
+--
+Word: rizz
+Definition: a term used for cocaine
+Example: "You wanna do some rizz?"
+--
+...
+"""
+
 import urllib.request, sys, json
 
 if __name__ == '__main__':
@@ -15,6 +34,7 @@ if __name__ == '__main__':
         entry["example"] = entry["example"].replace("[", "")
         entry["example"] = entry["example"].replace("]", "")
 
+        print(f"Word: {search_term}")
         print(f"Definition: {entry['definition']}")
         print(f"Example: {entry['example']}")
         print("--")
