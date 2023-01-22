8d9958dad96a581ed8c7ef498136f13c5104d65c
Edwin J <64994681+ed963@users.noreply.github.com>
2023-01-21 19:29:25 -0500

    Update query.py to support phrase as search term (#8)

--- utils/query.py
+++ utils/query.py
@@ -20,9 +20,10 @@ Example: "You wanna do some rizz?"
 import urllib.request, sys, json
 
 if __name__ == '__main__':
-    search_term = sys.argv[1]
+    search_term = " ".join(sys.argv[1:])
+    search_term_escaped = urllib.parse.quote(search_term)
 
-    response = urllib.request.urlopen(f"https://api.urbandictionary.com/v0/define?term={search_term}").read()
+    response = urllib.request.urlopen(f"https://api.urbandictionary.com/v0/define?term={search_term_escaped}").read()
     response_list = json.loads(response)["list"]
     
     for entry in response_list:
@@ -36,5 +37,5 @@ if __name__ == '__main__':
 
         print(f"Word: {search_term}")
         print(f"Definition: {entry['definition']}")
-        print(f"Example: {entry['example']}")
+        # print(f"Example: {entry['example']}")
         print("--")
